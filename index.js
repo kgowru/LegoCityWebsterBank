574442cce6efbf47670e6fb24ede94e951e726b9
kgowru <k.gowru@gmail.com>
2017-07-12 23:09:47 +0000

    pushing for cem

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ var express = require('express'),
 	http = require('http').Server(app),
 	io = require('socket.io')(http),
 	rc522 = require('rc522'),
-	jsforce = require('jsforce');
+	jsforce = require('jsforce'),
+	bodyParser = require('body-parser');
+	
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true,  parameterLimit: 1000000}));
 
-var sfConn = new jsforce.Connection({
+
+/*var sfConn = new jsforce.Connection({
 	loginUrl: 'https://corsa04-perfeng2-2015139045.vpod.t.force.com/'
 });
 
 sfConn.login("legocity@iot.com", "legolego", function(err, userInfo){
 	if (err) { return console.error(err); } 
 	console.log('logged into salesforce');
-});
+});*/
 
 app.use('/static', express.static(__dirname + '/public'))
 
@@ -20,11 +25,30 @@ app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/sendToIot', function(req, res, next){ 
+	console.log(JSON.stringify(req.body));
+	//var body = JSON.parse(req.body);
+	//console.log('prased body: ' + body);
+	if(!req.body.eventName){
+		console.log('no body, error');
+		res.status(400).send({error:'test'});
+	} else {
+		console.log('request has body');
+	console.log('hit my endpoint with name: ' + JSON.stringify(req.body.eventName) + ' and payload: ' + JSON.stringify(req.body.eventPayload));
+	/*sfConn.sobject(req.body.eventName).create(req.body.eventPayload, function(err, result){
+						if(err) return console.error(err);
+						console.log('checkin event sent to IoT Cloud');
+						});*/
+	
+	res.status(200).end();
+ }
+});
+
 io.on('connection', function(socket){
 	console.log('a user connected');
 	rc522(function(rfidSerialNumber){
 		if(rfidSerialNumber){
-			sfConn.query("SELECT Name, Id from Contact WHERE rfid__c =\'" + rfidSerialNumber + "\'", function(err, result){
+			sfConn.query("SELECT Name, Id from Contact WHERE rfid__c =\'" + rfidSerialNumber + "\' ORDER BY LastModifiedDate DESC", function(err, result){
 				if(err) { io.emit('rfid', ''); return console.error(err);} 
 				console.log('ths is the rfid: ' + rfidSerialNumber);
 				console.log('return salesforce respone: ' + JSON.stringify(result));
