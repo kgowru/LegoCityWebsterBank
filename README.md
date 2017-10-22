5c4b52097dc8fab425e0c29e2c152bc577a3e20b
Kapil Gowru <kapil.gowru@kapilgowru-ltm2.internal.salesforce.com>
2017-10-22 11:34:44 -0700

    updated app to auto send transaction

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
-# RFID Reader for IoT Lego City
-This is a node app running on a raspberry pi 3 that reads the RFID tag and then identifies the user from Salesforce
-ion
+# ATM App for Lego City Webster Bank
+This is a node app running on a raspberry pi 3 that simulates an ATM
 
-### Installation
-Please follow steps 1-4 on this github [readme](https://github.com/ocsacesar/rc522). 
 ### Getting Started
 Run the following in the terminal
 ```
-	npm install 
-	sudo node index.js
+	npm install
+	node index.js
 ```
-Open browser window to localhost:3000 for RFID to initialize
-
-**sudo is required for the RFID reader to work**
+Open browser window to localhost:3000
--- index.js
+++ index.js
@@ -5,19 +5,9 @@ var express = require('express'),
 	rc522 = require('rc522'),
 	jsforce = require('jsforce'),
 	bodyParser = require('body-parser');
-	
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true,  parameterLimit: 1000000}));
-
-
-/*var sfConn = new jsforce.Connection({
-	loginUrl: 'https://corsa04-perfeng2-2015139045.vpod.t.force.com/'
-});
-
-sfConn.login("legocity@iot.com", "legolego", function(err, userInfo){
-	if (err) { return console.error(err); } 
-	console.log('logged into salesforce');
-});*/
 
 app.use('/static', express.static(__dirname + '/public'))
 
@@ -25,54 +15,54 @@ app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/sendToIot', function(req, res, next){ 
-	console.log(JSON.stringify(req.body));
-	//var body = JSON.parse(req.body);
-	//console.log('prased body: ' + body);
-	if(!req.body.eventName){
-		console.log('no body, error');
-		res.status(400).send({error:'test'});
-	} else {
-		console.log('request has body');
-	console.log('hit my endpoint with name: ' + JSON.stringify(req.body.eventName) + ' and payload: ' + JSON.stringify(req.body.eventPayload));
-	/*sfConn.sobject(req.body.eventName).create(req.body.eventPayload, function(err, result){
-						if(err) return console.error(err);
-						console.log('checkin event sent to IoT Cloud');
-						});*/
-	
-	res.status(200).end();
- }
-});
-
-io.on('connection', function(socket){
-	console.log('a user connected');
-	rc522(function(rfidSerialNumber){
-		if(rfidSerialNumber){
-			sfConn.query("SELECT Name, Id from Contact WHERE rfid__c =\'" + rfidSerialNumber + "\' ORDER BY LastModifiedDate DESC", function(err, result){
-				if(err) { io.emit('rfid', ''); return console.error(err);} 
-				console.log('ths is the rfid: ' + rfidSerialNumber);
-				console.log('return salesforce respone: ' + JSON.stringify(result));
-				if(result && result.records && result.records.length !== 0 && result.records[0].Name) {
-					io.emit('rfid', result.records[0].Name); 
-					sfConn.sobject('checkin__e').create({
-						location__c: 'restaurant',
-						userid__c:result.records[0].Id,
-						time__c: new Date().toISOString()
-						}, function(err, result){
-						if(err) return console.error(err);
-						console.log('checkin event sent to IoT Cloud');
-						});
-				}
-					
-				else 
-					io.emit('rfid', null);
-			});
-			
-			//send IoT cloud request here
-		}
-		
-	});
-});
+// app.post('/sendToIot', function(req, res, next){
+// 	console.log(JSON.stringify(req.body));
+// 	//var body = JSON.parse(req.body);
+// 	//console.log('prased body: ' + body);
+// 	if(!req.body.eventName){
+// 		console.log('no body, error');
+// 		res.status(400).send({error:'test'});
+// 	} else {
+// 		console.log('request has body');
+// 	console.log('hit my endpoint with name: ' + JSON.stringify(req.body.eventName) + ' and payload: ' + JSON.stringify(req.body.eventPayload));
+// 	/*sfConn.sobject(req.body.eventName).create(req.body.eventPayload, function(err, result){
+// 						if(err) return console.error(err);
+// 						console.log('checkin event sent to IoT Cloud');
+// 						});*/
+//
+// 	res.status(200).end();
+//  }
+// });
+//
+// io.on('connection', function(socket){
+// 	console.log('a user connected');
+// 	rc522(function(rfidSerialNumber){
+// 		if(rfidSerialNumber){
+// 			sfConn.query("SELECT Name, Id from Contact WHERE rfid__c =\'" + rfidSerialNumber + "\' ORDER BY LastModifiedDate DESC", function(err, result){
+// 				if(err) { io.emit('rfid', ''); return console.error(err);}
+// 				console.log('ths is the rfid: ' + rfidSerialNumber);
+// 				console.log('return salesforce respone: ' + JSON.stringify(result));
+// 				if(result && result.records && result.records.length !== 0 && result.records[0].Name) {
+// 					io.emit('rfid', result.records[0].Name);
+// 					sfConn.sobject('checkin__e').create({
+// 						location__c: 'restaurant',
+// 						userid__c:result.records[0].Id,
+// 						time__c: new Date().toISOString()
+// 						}, function(err, result){
+// 						if(err) return console.error(err);
+// 						console.log('checkin event sent to IoT Cloud');
+// 						});
+// 				}
+//
+// 				else
+// 					io.emit('rfid', null);
+// 			});
+//
+// 			//send IoT cloud request here
+// 		}
+//
+// 	});
+// });
 
 http.listen(3000, function() {
 console.log('listening on port 3000');
